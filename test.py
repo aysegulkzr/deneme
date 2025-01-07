0570a38f4f82eacf42bf4b1a6fb572b9dd1a72a3
aysegulkzr <aysegul.kizir@ogr.ksbu.edu.tr>
2025-01-07 23:14:42 +0300

    uc sayi

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 # Kullanıcıdan iki sayı al
 sayi1 = float(input("Birinci sayıyı girin: "))
 sayi2 = float(input("İkinci sayıyı girin: "))
-
+sayi3 =float(input("ucuncu sayıyı girin: "))
 # Sayıları çarp
-carpim = sayi1 * sayi2
+carpim = sayi1 * sayi2*sayi3
 
 # Sonucu ekrana yazdır
 print(f"Girdiğiniz sayıların çarpımı: {carpim}")
