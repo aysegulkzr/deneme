7965c18afbad1a40affc09f9324524fe0f138e90
aysegulkzr <aysegul.kizir@ogr.ksbu.edu.tr>
2025-01-08 22:53:11 +0300

    Add files via upload

---
+++ .github/workflows/test.py
@@ -0,0 +1,10 @@
+# Kullanıcıdan iki sayı al
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+sayi3 =float(input("ucuncu sayıyı girin: "))
+# Sayıları çarp
+toplam = sayi1 + sayi2
+toplam= toplam +sayi3
+
+# Sonucu ekrana yazdır
+print(f"Girdiğiniz sayıların toplamı: {toplam}")
